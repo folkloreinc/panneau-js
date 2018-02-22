5ecac42c70430b461974d022cdd14c94ceed1866
Hugo St-Onge <hso@atelierfolklore.ca>
2018-02-22 13:18:52 -0500

    @panneau/field-items: export additional components

--- fields/items/src/index.js
+++ fields/items/src/index.js
@@ -1,3 +1,9 @@
 import ItemsField from './ItemsField';
+import AddButton from './AddButton';
+import ButtonGroup from './ButtonGroup';
 
+export {
+    AddButton,
+    ButtonGroup,
+};
 export default ItemsField;
