2a21423b54b76ff8ee54858276c3139b2a2d6ad9
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-03-05 18:17:11 -0500

    Fix with babel and scss

--- build/process-scss.js
+++ build/process-scss.js
@@ -1,10 +1,15 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const sass = require('node-sass');
+const path = require('path');
 
 module.exports = (data, filename) => {
     const result = sass.renderSync({
         data,
         file: filename,
+        includePaths: [
+            path.join(process.env.PWD, './node_modules'),
+            path.join(__dirname, '../node_modules'),
+        ],
     }).css;
     return result.toString('utf8');
 };
