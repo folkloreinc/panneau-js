ee7715a42b758bc30ae3cdc1582ec5406a0fa38e
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-03-05 19:00:13 -0500

    Fix with imports

--- build/process-scss.js
+++ build/process-scss.js
@@ -1,14 +1,22 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-const sass = require('node-sass');
 const path = require('path');
+/* eslint-disable import/no-extraneous-dependencies */
+const sass = require('node-sass');
+const tildeImporter = require('node-sass-tilde-importer');
+/* eslint-disable import/no-extraneous-dependencies */
 
 module.exports = (data, filename) => {
     const result = sass.renderSync({
         data,
         file: filename,
+        importer: tildeImporter,
         includePaths: [
             path.join(process.env.PWD, './node_modules'),
             path.join(__dirname, '../node_modules'),
+            path.join(__dirname, '../fields/select/node_modules'),
+            path.join(__dirname, '../fields/fields/node_modules'),
+            path.join(__dirname, '../layouts/layouts/node_modules'),
+            path.join(__dirname, '../lists/lists/node_modules'),
+            path.join(__dirname, '../modals/modals/node_modules'),
         ],
     }).css;
     return result.toString('utf8');
