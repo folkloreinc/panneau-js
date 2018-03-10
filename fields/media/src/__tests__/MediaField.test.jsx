2993203794ca87f10e839cf045144ee6a1cb3924
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-03-10 17:00:22 -0500

    Fix with tests

--- fields/media/src/__tests__/MediaField.test.jsx
+++ fields/media/src/__tests__/MediaField.test.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 /* eslint-disable import/no-extraneous-dependencies */
 import renderer from 'react-test-renderer';
 /* eslint-enable import/no-extraneous-dependencies */
+import { IntlProvider } from 'react-intl';
 import MediaField from '../MediaField';
 import MediaDocument from '../MediaDocument';
 
 test('match snapshot', () => {
     const component = renderer.create((
-        <MediaField
-            label="Label"
-            MediaComponent={MediaDocument}
-        />
+        <IntlProvider locale="en">
+            <MediaField
+                label="Label"
+                MediaComponent={MediaDocument}
+            />
+        </IntlProvider>
     ));
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
