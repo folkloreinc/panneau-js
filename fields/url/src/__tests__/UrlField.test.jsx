55f41cac1634687271311e76cb4e0edd878b97d3
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-05-18 16:21:45 -0400

    Fix with tests

--- fields/url/src/__tests__/UrlField.test.jsx
+++ fields/url/src/__tests__/UrlField.test.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+// import { shallow } from 'enzyme';
 // import sinon from 'sinon';
 import UrlField from '../UrlField';
 
@@ -14,12 +14,4 @@ test('match snapshot', () => {
     ));
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
-});
-
-test('value is in <input />', () => {
-    const field = shallow(<UrlField value="https://google.com" />);
-    expect(field.find('input').prop('value')).toEqual('google.com');
-
-    const nullField = shallow(<UrlField value={null} />);
-    expect(nullField.find('input').prop('value')).toEqual('');
 });
