480f22e1ce639b60882e47a24961118182930007
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2018-07-05 15:46:20 -0400

    Added form to context and added withForm to access it

---
+++ forms/core/src/PropTypes.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+export const errors = PropTypes.oneOfType([
+    PropTypes.objectOf(PropTypes.string),
+    PropTypes.objectOf(PropTypes.arrayOf(PropTypes.string)),
+]);
+
+export const form = PropTypes.shape({
+    errors,
+    value: PropTypes.object,
+});
