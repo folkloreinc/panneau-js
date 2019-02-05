6a3a7f616ab2d093d8e27db5dba577a953c40c4a
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2019-02-05 12:19:00 -0500

    Added component preview

--- previews/previews/src/items.js
+++ previews/previews/src/items.js
@@ -1,8 +1,10 @@
 import { ComponentsCollection } from '@panneau/core';
 import Json from '@panneau/preview-json';
+import Component from '@panneau/preview-component';
 
 const collection = new ComponentsCollection({
     Json,
+    Component,
 });
 
 export default collection;
