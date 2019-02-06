1dff33d3e5cf0adeecd2a986c5ae3da9bbac3d37
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2019-02-06 16:01:15 -0500

    Fix with form component

--- packages/generator-panneau/src/generators/form-package/templates/Component.jsx
+++ packages/generator-panneau/src/generators/form-package/templates/Component.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import isObject from 'lodash/isObject';
+import { defineMessages } from 'react-intl';
+import { PropTypes as PanneauPropTypes, ErrorsComponent as Errors } from '@panneau/core';
 import { FieldsGroup } from '@panneau/field';
+import { PropTypes as FormPropTypes, FormActions, messages } from '@panneau/form';
 
 import styles from './styles.scss';
 
@@ -9,22 +13,17 @@ const propTypes = {
     action: PropTypes.string,
     method: PropTypes.string,
     fields: PropTypes.arrayOf(PropTypes.object),
-    value: PropTypes.shape({}),
-    errors: PropTypes.objectOf(PropTypes.array),
-    buttons: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string,
-        type: PropTypes.string,
-        label: PropTypes.string,
-        className: PropTypes.string,
-        onClick: PropTypes.func,
-    })),
-    submitForm: PropTypes.shape({
-        then: PropTypes.func.isRequired,
-    }),
-    onValueChange: PropTypes.func,
+    value: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+    errors: FormPropTypes.errors,
+    generalErrorMessage: PropTypes.string,
+    buttons: PanneauPropTypes.buttons,
+    submitForm: PropTypes.func,
+    onChange: PropTypes.func,
     onSubmit: PropTypes.func,
     onErrors: PropTypes.func,
     onComplete: PropTypes.func,
+    readOnly: PropTypes.bool,
+    notice: FormPropTypes.notice,
 };
 
 const defaultProps = {
@@ -34,28 +33,53 @@ const defaultProps = {
         {
             id: 'submit',
             type: 'submit',
-            label: 'OK',
-            className: classNames({
-                'btn-primary': true,
-            }),
+            label: messages.save,
+            className: 'btn-primary',
         },
     ],
     fields: [],
     value: null,
     errors: null,
+    notice: null,
     submitForm: null,
-    onValueChange: null,
+    onChange: null,
     onSubmit: null,
     onErrors: null,
     onComplete: null,
 };
 
+const childContextTypes = {
+    form: FormPropTypes.form,
+};
+
+const contextTypes = {
+    form: FormPropTypes.form,
+};
+
 class <%= componentName %> extends Component {
+    static getDerivedStateFromProps(props, state) {
+        const isValueControlled = props.onChange !== null;
+        const isErrorsControlled = props.onErrors !== null;
+        const valueChanged = isValueControlled && props.value !== state.value;
+        const errorsChanged = isErrorsControlled && props.errors !== state.errors;
+        if (valueChanged || errorsChanged) {
+            return {
+                value: valueChanged ? props.value : state.value,
+                errors: errorsChanged ? props.errors : state.errors,
+            };
+        }
+        return null;
+    }
+
     constructor(props) {
         super(props);
 
-        this.onFieldsValueChange = this.onFieldsValueChange.bind(this);
+        this.onFieldsChange = this.onFieldsChange.bind(this);
         this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.onFormSubmitComplete = this.onFormSubmitComplete.bind(this);
+        this.onFormSubmitError = this.onFormSubmitError.bind(this);
+
+        this.refForm = React.createRef();
 
         this.state = {
             value: props.value,
@@ -63,50 +87,29 @@ class <%= componentName %> extends Component {
         };
     }
 
-    componentWillReceiveProps({
-        value: nextValue,
-        errors: nextErrors,
-    }) {
-        const { value, errors } = this.props;
-        const valueChanged = nextValue !== value;
-        if (valueChanged) {
-            this.setState({
-                value: nextValue,
-            });
-        }
-
-        const errorsChanged = nextErrors !== errors;
-        if (errorsChanged) {
-            this.setState({
-                errors: nextErrors,
-            });
-        }
+    getChildContext() {
+        const { value, errors } = this.state;
+        const { form = null } = this.context;
+        return {
+            form: form !== null ? form : {
+                errors,
+                value,
+            },
+        };
     }
 
-    onFieldsValueChange(value) {
-        const { onValueChange } = this.props;
-        if (onValueChange !== null) {
-            onValueChange(value);
-        } else {
-            this.setState({
-                value,
-            });
-        }
+    onFieldsChange(value) {
+        this.setValue(value);
     }
 
     onFormSubmit(e) {
-        const { onSubmit, submitForm } = this.props;
+        const { onSubmit } = this.props;
         const { value } = this.state;
-
         if (onSubmit !== null) {
             onSubmit(e, value);
-        }
-
-        if (submitForm !== null) {
+        } else {
             e.preventDefault();
-            submitForm(value)
-                .then(this.onFormSubmitComplete)
-                .then(this.onFormSubmitError);
+            this.submit();
         }
     }
 
@@ -117,7 +120,22 @@ class <%= componentName %> extends Component {
         }
     }
 
-    onFormSubmitError(errors) {
+    onFormSubmitError(error) {
+        const { generalErrorMessage } = this.props;
+        if (error.name === 'ValidationError') {
+            this.setErrors(error.responseData);
+        } else {
+            this.setErrors(generalErrorMessage);
+        }
+    }
+
+    /**
+     * Set the errors
+     *
+     * @param {object} errors
+     * @public
+     */
+    setErrors(errors) {
         const { onErrors } = this.props;
         if (onErrors !== null) {
             onErrors(errors);
@@ -128,87 +146,77 @@ class <%= componentName %> extends Component {
         }
     }
 
-    renderFields() {
-        const { fields } = this.props;
-        const { value, errors } = this.state;
-        return (
-            <FieldsGroup
-                fields={fields}
-                value={value}
-                errors={errors}
-                onChange={this.onFieldsValueChange}
-            />
-        );
+    /**
+     * Set the value
+     *
+     * @param {object} value
+     * @public
+     */
+    setValue(value) {
+        const { onChange } = this.props;
+        if (onChange !== null) {
+            onChange(value);
+        } else {
+            this.setState({
+                value,
+            });
+        }
     }
 
-    renderActions() {
-        const { buttons } = this.props;
-        return (
-            <div className={styles.buttons}>
-                { buttons.map(({
-                    id,
-                    label,
-                    type,
-                    className,
-                    onClick,
-                }) => (
-                    <button
-                        key={`actions-button-${id}`}
-                        type={type}
-                        className={classNames({
-                            btn: true,
-                            [className]: (className || null) !== null,
-                            [styles.button]: true,
-                            [styles[type]]: true,
-                        })}
-                        onClick={onClick || null}
-                    >
-                        { label }
-                    </button>
-                )) }
-
-            </div>
-        );
+    /**
+     * Submit the form
+     *
+     * @public
+     */
+    submit() {
+        const { submitForm } = this.props;
+        const { value } = this.state;
+        if (submitForm !== null) {
+            submitForm(value)
+                .then(this.onFormSubmitComplete)
+                .catch(this.onFormSubmitError);
+        } else {
+            this.refForm.current.submit();
+        }
     }
 
     render() {
         const {
-            action,
-            method,
+            action, method, fields, readOnly, notice, buttons,
         } = this.props;
+        const { value, errors } = this.state;
 
-        const containerClassNames = classNames({
-            [styles.container]: true,
-        });
-
-        const formClassNames = classNames({
-            [styles.form]: true,
-        });
-
-        const fieldsClassNames = classNames({
-            [styles.fields]: true,
-        });
-
-        const actionsClassNames = classNames({
-            [styles.actions]: true,
-        });
+        const errorsIsGeneral = errors !== null && !isObject(errors);
 
         return (
-            <div className={containerClassNames}>
-                <div className={formClassNames}>
-                    <form
-                        action={action}
-                        method={method}
-                        onSubmit={this.onFormSubmit}
-                    >
-                        <div className={fieldsClassNames}>
-                            { this.renderFields() }
-                        </div>
-                        <div className={actionsClassNames}>
-                            { this.renderActions() }
-                        </div>
-                    </form>
-                </div>
+            <div className={styles.container}>
+                <form
+                    action={action}
+                    method={method}
+                    onSubmit={this.onFormSubmit}
+                    className={styles.form}
+                    ref={this.refForm}
+                >
+                    {errorsIsGeneral ? (
+                        <Errors className={styles.errors} errors={errors} />
+                    ) : null}
+
+                    <div className={styles.fields}>
+                        <FieldsGroup
+                            readOnly={readOnly}
+                            fields={fields}
+                            value={value}
+                            errors={!errorsIsGeneral ? errors : null}
+                            onChange={this.onFieldsChange}
+                        />
+                    </div>
+
+                    <FormActions
+                        notice={notice}
+                        buttons={buttons}
+                        className={classNames(['mt-4', 'border-top', 'pt-2', styles.actions])}
+                    />
+                </form>
             </div>
         );
     }
@@ -216,5 +224,7 @@ class <%= componentName %> extends Component {
 
 <%= componentName %>.propTypes = propTypes;
 <%= componentName %>.defaultProps = defaultProps;
+<%= componentName %>.childContextTypes = childContextTypes;
+<%= componentName %>.contextTypes = childContextTypes;
 
 export default <%= componentName %>;
