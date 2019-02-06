492c0a735ea2aba75e6b20871452bf1b5ec04910
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2019-02-06 16:00:46 -0500

    Added preview form

--- forms/forms/src/items.js
+++ forms/forms/src/items.js
@@ -1,8 +1,10 @@
 import { ComponentsCollection } from '@panneau/core';
 import Normal from '@panneau/form-normal';
+import Preview from '@panneau/form-preview';
 
 const collection = new ComponentsCollection({
     Normal,
+    Preview,
 });
 
 export default collection;
