5d3eb63d38c9296ed78f4532ff8792a0e5fb3d33
Nicolas Roy B <nrb@atelierfolklore.ca>
2021-05-20 10:55:10 -0400

    Fix

--- packages/forms/src/components.js
+++ packages/forms/src/components.js
@@ -1,5 +1,4 @@
 // Import forms
-
 export { default as Normal } from '@panneau/form-normal';
 export { default as Inline } from '@panneau/form-inline';
 export { default as Horizontal } from '@panneau/form-horizontal';
