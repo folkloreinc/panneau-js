61e0a382fa54dcce2340e3b054cc8af4367b29f5
Nicolas Roy B <nrb@atelierfolklore.ca>
2021-06-15 13:02:25 -0400

    Added displays

--- packages/displays/src/components.js
+++ packages/displays/src/components.js
@@ -1,3 +1,6 @@
 export { default as Text } from '@panneau/display-text';
 export { default as TextLocalized } from '@panneau/display-text-localized';
 export { default as Boolean } from '@panneau/display-boolean';
+export { default as Image } from '@panneau/display-image';
+export { default as Label } from '@panneau/display-label';
+export { default as Select } from '@panneau/display-select';
