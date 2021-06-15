dbbfe99b44cbb527bde132e7db5ca934517fbca1
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2021-06-15 18:07:46 -0400

    Fix with flatten

--- packages/core/src/utils/flattenComponents.js
+++ packages/core/src/utils/flattenComponents.js
@@ -5,14 +5,16 @@ const flattenComponents = (components, prefix = null) => {
         return null;
     }
     return Object.keys(components).reduce(
-        (newMap, key) => ({
-            ...newMap,
-            [prefix !== null ? `${prefix}.${key}` : key]: ReactIs.isValidElementType(
-                components[key],
-            )
-                ? components[key]
-                : flattenComponents(components[key], key),
-        }),
+        (newMap, key) =>
+            ReactIs.isValidElementType(components[key])
+                ? {
+                      ...newMap,
+                      [prefix !== null ? `${prefix}.${key}` : key]: components[key],
+                  }
+                : {
+                      ...newMap,
+                      ...flattenComponents(components[key], key),
+                  },
         {},
     );
 };
