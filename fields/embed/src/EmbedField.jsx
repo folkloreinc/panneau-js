ee9ac68116fb6d8b6940cef273f7c554b3cf4b73
Nicolas Roy B <nrb@atelierfolklore.ca>
2021-06-16 15:04:13 -0400

    Basic embed

--- fields/embed/src/EmbedField.jsx
+++ fields/embed/src/EmbedField.jsx
@@ -1,9 +1,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import Card from '@panneau/element-card';
 import TextField from '@panneau/field-text';
 import isEmpty from 'lodash/isEmpty';
+import isNumber from 'lodash/isNumber';
+import isObject from 'lodash/isObject';
 import isString from 'lodash/isString';
 import PropTypes from 'prop-types';
 import React, { useCallback, useMemo } from 'react';
+import { FormattedMessage } from 'react-intl';
 
 const getScheme = (url, schemesPattern) => {
     const match = url !== null && isString(url) ? url.match(schemesPattern) : null;
@@ -17,12 +21,21 @@ const withScheme = (url, prefix, schemesPattern) =>
     url !== null && isString(url) && !url.match(schemesPattern) ? `${prefix}${url}` : url;
 
 const propTypes = {
-    value: PropTypes.string,
+    value: PropTypes.shape({
+        url: PropTypes.string,
+        metadata: PropTypes.shape({
+            title: PropTypes.string,
+            providerName: PropTypes.string,
+            authorName: PropTypes.string,
+            thumbnailUrl: PropTypes.string,
+            description: PropTypes.string,
+            duration: PropTypes.number,
+            width: PropTypes.number,
+            height: PropTypes.number,
+        }),
+    }),
     schemes: PropTypes.arrayOf(PropTypes.string),
     url: PropTypes.string,
-    metadata: PropTypes.shape({
-        provider_name: PropTypes.string,
-    }),
     disabled: PropTypes.bool,
     className: PropTypes.string,
     onChange: PropTypes.func,
@@ -32,23 +45,42 @@ const defaultProps = {
     value: null,
     schemes: ['http://', 'https://', 'ftp://'],
     url: null,
-    metadata: null,
     disabled: null,
     className: null,
     onChange: null,
 };
 
-const EmbedField = ({ value, schemes, url, disabled, className, onChange, ...props }) => {
+const EmbedField = ({
+    value,
+    schemes,
+    url: prefixUrl,
+    disabled,
+    className,
+    onChange,
+    ...props
+}) => {
+    const { url = null, metadata = null } = isObject(value) ? value : { url: value };
+    const {
+        title = null,
+        providerName = null,
+        authorName = null,
+        thumbnailUrl = null,
+        description = null,
+        duration = null,
+        width = null,
+        height = null,
+    } = metadata || {};
+
     const schemesPattern = useMemo(() => new RegExp(`^(${schemes.join('|')})`, 'i'), [schemes]);
 
     const scheme = useMemo(
-        () => getScheme(value, schemesPattern) || schemes[0],
-        [value, schemes, schemesPattern],
+        () => getScheme(url, schemesPattern) || schemes[0],
+        [url, schemes, schemesPattern],
     );
 
     const valueWithoutScheme = useMemo(
-        () => removeScheme(value, schemesPattern),
-        [value, schemesPattern],
+        () => removeScheme(url, schemesPattern),
+        [url, schemesPattern],
     );
 
     const onFieldChange = useCallback(
@@ -63,13 +95,49 @@ const EmbedField = ({ value, schemes, url, disabled, className, onChange, ...pro
         [onChange, scheme, schemesPattern],
     );
 
-    return (
+    const onClose = useCallback(() => {
+        onChange(null);
+    }, [onChange]);
+
+    const dateDuration = isNumber(duration)
+        ? new Date(duration * 1000).toISOString().substr(11, 8)
+        : null;
+
+    return metadata !== null ? (
+        <Card title={`${providerName} - ${title}`} image={thumbnailUrl} onClose={onClose}>
+            <div className="my-3">
+                {!isEmpty(authorName) ? <p className="fw-bold lh-1 mb-1">{authorName}</p> : null}
+                {!isEmpty(description) ? <p className="lh-1 mb-1">{description}</p> : null}
+                {!isEmpty(url) ? (
+                    <p className="lh-1 mb-3">
+                        <a href={url} target="_blank" rel="noopener noreferrer">
+                            {url}
+                        </a>
+                    </p>
+                ) : null}
+                {dateDuration !== null ? (
+                    <p className="lh-1 mb-1">
+                        <FormattedMessage
+                            defaultMessage="Duration:"
+                            description="Field description"
+                        />{' '}
+                        {dateDuration}
+                    </p>
+                ) : null}
+                {width !== null && height !== null ? (
+                    <p className="lh-1 mb-1">
+                        {width}x{height}
+                    </p>
+                ) : null}
+            </div>
+        </Card>
+    ) : (
         <TextField
             {...props}
             className={className}
             value={valueWithoutScheme}
             onChange={onFieldChange}
-            prepend={url || scheme}
+            prepend={prefixUrl || scheme}
             disabled={disabled}
             type="text"
         />
--- fields/embed/src/_stories/Embed.stories.jsx
+++ fields/embed/src/_stories/Embed.stories.jsx
@@ -13,11 +13,33 @@ export default {
 // eslint-disable-next-line react/prop-types
 const Container = ({ value: initialValue, ...props }) => {
     const [value, setValue] = useState(initialValue);
-    return <EmbedField {...props} value={value} onChange={setValue} />;
+    return (
+        <EmbedField {...props} value={value} onChange={setValue} onClose={() => setValue(null)} />
+    );
 };
 
-export const Normal = () => <Container />;
+export const Normal = () => (
+    <Container value="https://www.youtube.com/watch?v=Xw5AiRVqfqk&t=1058s" />
+);
 
-export const WithMetadata = () => (
-    <Container url="https://www.youtube.com/watch?v=Xw5AiRVqfqk&t=1058s" />
+export const WithValue = () => (
+    <Container value={{ url: 'https://www.youtube.com/watch?v=Xw5AiRVqfqk&t=1058s' }} />
+);
+
+export const WithMeta = () => (
+    <Container
+        value={{
+            url: 'https://www.youtube.com/watch?v=Xw5AiRVqfqk&t=1058s',
+            metadata: {
+                title: 'Un vidéo cool',
+                providerName: 'YouTube',
+                authorName: 'Paul',
+                thumbnailUrl: 'https://i.vimeocdn.com/video/1163787851_640',
+                description: 'Une description',
+                duration: 187,
+                width: 1920,
+                height: 1080,
+            },
+        }}
+    />
 );
