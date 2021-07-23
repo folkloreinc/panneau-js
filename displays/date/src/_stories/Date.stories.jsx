b670657859f3a8af78f8bf25093fbc426f2c7b4e
Nicolas Roy B <nrb@atelierfolklore.ca>
2021-07-22 23:55:58 -0400

    Japanese dates

--- displays/date/src/_stories/Date.stories.jsx
+++ displays/date/src/_stories/Date.stories.jsx
@@ -10,3 +10,11 @@ export default {
 };
 
 export const Normal = () => <DateDisplay value="2021-07-16T00:00:00-04:00" />;
+
+export const French = () => (
+    <DateDisplay value="2021-07-16T00:00:00-04:00" format="LLLL" locale="fr" />
+);
+
+export const Japanese = () => (
+    <DateDisplay value="2021-07-16T00:00:00-04:00" format="LLLL" locale="ja" />
+);
