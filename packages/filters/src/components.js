3e9d640b58c1b0fadccab4927935904a1ec5b70e
Nicolas Roy B <phatshambler@gmail.com>
2023-03-31 16:33:44 -0400

    Import new filter

--- packages/filters/src/components.js
+++ packages/filters/src/components.js
@@ -4,4 +4,4 @@ export { default as Radios } from '@panneau/filter-radios';
 export { default as Search } from '@panneau/filter-search';
 export { default as Select } from '@panneau/filter-select';
 export { default as Toggle } from '@panneau/filter-toggle';
-// export { default as Label } from '@panneau/filter-label';
+export { default as Label } from '@panneau/filter-label';
