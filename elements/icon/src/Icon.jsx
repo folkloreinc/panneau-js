7cd982e1f76a4d6fd07f83797516be9f095671cb
Nicolas Roy B <phatshambler@gmail.com>
2023-07-24 14:58:10 -0400

    Export fix

--- elements/icon/src/Icon.jsx
+++ elements/icon/src/Icon.jsx
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import styles from './styles.module.scss';
-import 'bootstrap-icons/font/bootstrap-icons.scss';
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const propTypes = {
     name: PropTypes.string.isRequired,
