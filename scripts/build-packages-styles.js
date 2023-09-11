273e11a264be9f3a6c693c62a94c22588f2a92bb
Nicolas Roy B. <phatshambler@gmail.com>
2023-09-11 16:09:02 -0400

    Fix styles export

--- scripts/build-packages-styles.js
+++ scripts/build-packages-styles.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const ejs = require('ejs');
 const path = require('path');
 const fs = require('fs');
+const ejs = require('ejs');
 const fsExtra = require('fs-extra');
 const { program } = require('commander');
 
@@ -21,18 +21,24 @@ program.parse(process.argv);
 
 const { pattern } = program.opts();
 
-const nodeModulesPath = path.join(process.cwd(), './node_modules/');
 const packageJsonPath = path.join(process.cwd(), 'package.json');
 const { dependencies } = fsExtra.readJsonSync(packageJsonPath);
 
+console.log('dependencies', Object.keys(dependencies));
+
 const patternRegExp = new RegExp(`^${pattern.replace('*', '.*')}$`, 'i');
-const packages = Object.keys(dependencies).filter(
-    (it) =>
-        patternRegExp.test(it) &&
-        fs.existsSync(
-            path.resolve(nodeModulesPath, `${it}/assets/css/styles.css`),
-        ),
-);
+const packages = Object.keys(dependencies).filter((it) => {
+    if (!patternRegExp.test(it)) {
+        return false;
+    }
+    try {
+        require.resolve(`${it}/assets/css/styles.css`);
+    } catch (e) {
+        console.log(e);
+        return false;
+    }
+    return true;
+});
 
 const templateStr = fs.readFileSync(templateFile);
 const styles = ejs.render(templateStr.toString('utf-8'), {
