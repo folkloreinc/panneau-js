7c023addf33ff49a76c6ad165ce82b5552f595a4
Nicolas Roy B. <phatshambler@gmail.com>
2023-09-12 11:48:16 -0400

    Fixes

--- fields/item/src/ItemField.jsx
+++ fields/item/src/ItemField.jsx
@@ -285,6 +285,7 @@ const ItemField = ({
                 });
             } else {
                 onCreate(newValue);
+                loadOptions();
             }
         },
         [onCreate, loadOptions],
