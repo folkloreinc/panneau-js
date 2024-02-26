230faebd1086e3186fcbe991170b3d22c57a2f8e
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2024-02-26 12:48:39 -0500

    Fix

--- displays/date/src/Date.jsx
+++ displays/date/src/Date.jsx
@@ -1,24 +1,18 @@
 import formatDate from 'date-fns/format';
-import { de, enUS, es, frCA, ja } from 'date-fns/locale';
 import parse from 'date-fns/parse';
 import parseISO from 'date-fns/parseISO';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useIntl } from 'react-intl';
 
-const formats = {
-    fr: frCA,
-    de,
-    ja,
-    es,
-    en: enUS,
-};
+import { loadPackage } from '@panneau/core/utils';
 
 const propTypes = {
     value: PropTypes.string,
     format: PropTypes.string,
     parseFormat: PropTypes.string,
     locale: PropTypes.string,
+    localeLoaders: PropTypes.objectOf(PropTypes.func),
 };
 
 const defaultProps = {
@@ -26,27 +20,47 @@ const defaultProps = {
     format: 'yyyy-MM-dd',
     parseFormat: null,
     locale: null,
+    localeLoaders: {
+        fr: () => loadPackage('date-fns/locale/fr-CA', () => import('date-fns/locale/fr-CA')),
+        de: () => loadPackage('date-fns/locale/de', () => import('date-fns/locale/de')),
+        ja: () => loadPackage('date-fns/locale/ja', () => import('date-fns/locale/ja')),
+        es: () => loadPackage('date-fns/locale/es', () => import('date-fns/locale/es')),
+        en: () => loadPackage('date-fns/locale/en-US', () => import('date-fns/locale/en-US')),
+    },
 };
 
-const DateDisplay = ({ value, format, parseFormat, locale }) => {
+const DateDisplay = ({ value, format, parseFormat, locale, localeLoaders }) => {
     const { locale: defaultLocale } = useIntl();
     const finalLocale = locale || defaultLocale;
-    const options =
-        finalLocale !== null && typeof formats[finalLocale] !== 'undefined'
-            ? { locale: formats[finalLocale] }
-            : {};
-    let date = null;
+    const [localePackage, setLocalePackage] = useState(null);
+    useEffect(() => {
+        if (typeof localeLoaders[finalLocale] !== 'undefined') {
+            localeLoaders[finalLocale]().then(({ default: newLocalePackage }) =>
+                setLocalePackage(newLocalePackage),
+            );
+        }
+    }, [finalLocale, localeLoaders]);
 
-    try {
-        const parsed =
-            parseFormat !== null ? parse(value, parseFormat, new Date()) : parseISO(value);
-        date = parsed ? formatDate(parsed, format, options) : null;
-    } catch {
-        console.error('An error occured parsing or formatting date');
-        date = value !== null ? value : null;
-    }
+    const date = useMemo(() => {
+        let newDate = null;
+        try {
+            const parsed =
+                parseFormat !== null ? parse(value, parseFormat, new Date()) : parseISO(value);
+            newDate = parsed
+                ? formatDate(
+                      parsed,
+                      format,
+                      localePackage !== null ? { locale: localePackage } : {},
+                  )
+                : null;
+        } catch {
+            console.error('An error occured parsing or formatting date');
+            newDate = value !== null ? value : null;
+        }
+        return newDate;
+    }, [localePackage, value, format, parseFormat]);
 
-    return <div>{date !== null ? date : null}</div>;
+    return <div>{date}</div>;
 };
 
 DateDisplay.propTypes = propTypes;
