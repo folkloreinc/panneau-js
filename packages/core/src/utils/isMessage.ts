42a6dffac7813aea0d3ec939d014bf4883c531d1
David Mongeau-Petitpas <dmongeau.petitpas@gmail.com>
2026-07-13 12:46:38 -0400

    Fix with isMessage

--- packages/core/src/utils/isMessage.ts
+++ packages/core/src/utils/isMessage.ts
@@ -3,7 +3,11 @@ import isObject from 'lodash-es/isObject';
 import type { Message } from '../types';
 
 function isMessage(message: unknown): message is Message {
-    return isObject(message) && typeof (message as Message).defaultMessage !== 'undefined';
+    return (
+        isObject(message) &&
+        (typeof (message as Message).defaultMessage !== 'undefined' ||
+            typeof (message as Message).id !== 'undefined')
+    );
 }
 
 export default isMessage;
